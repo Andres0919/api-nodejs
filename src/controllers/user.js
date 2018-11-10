66b13ff692aed2212f584582b2ea0c5495ebf86e
Andres0919 <andres.posada0919@gmail.com>
2018-11-10 17:53:02 -0500

    creating nodejs api cap 3

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,11 +1,30 @@
 const User = require('../models/user');
 
 module.exports = {
-    index: async () => {
+    index: async (req, res, next) => {
         const users = await User.find({});
-        res.json(users);
+        res.status(200).json(users);
     },
-    newUser: () => {
-
+    newUser: async (req, res, next) => {
+        const newUser = new User(req.body);
+        const user = await newUser.save();
+        res.status(200).json(user);
+    },
+    getUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+        res.status(200).json(user);
+    },
+    replaceUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const newUser = req.body;
+        const oldUser = await User.findByIdAndUpdate(userId, newUser);
+        res.status(200).json({success: true});
+    },
+    updateUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const newUser = req.body;
+        const oldUser = await User.findByIdAndUpdate(userId, newUser);
+        res.status(200).json({success: true});
     }
 }
